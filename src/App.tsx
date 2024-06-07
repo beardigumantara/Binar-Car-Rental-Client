bc87a34a01b6ce5ecde05587ca266247865ddad2
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-06-07 07:13:29 +0700

    refactor: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
+// import './App.css'
 import Home from './pages/home'
+import Cars from './pages/cars'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <div>
-        <Home />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cars" element={<Cars />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/cars.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+import React from "react";
+
+const Cars :React.FC = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Cars</h1>
+            <p>this is Cars Page</p>
+            <Footer />
+        </div>
+    )
+}
+
+export default Cars; 
